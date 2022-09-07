20df2b18b9e9319c9f04c23b1dc369c8cae4c666
eliseu gili <gili.eliseup@gmail.com>
2022-09-07 13:14:59 -0300

    lwc

--- TCC/force-app/main/default/lwc/documentacao/documentacao.js
+++ TCC/force-app/main/default/lwc/documentacao/documentacao.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class Documentacao extends LightningElement {}
+const columns = [
+    { label: 'OSC', fieldName: 'nomeOSC', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },
+    { label: 'Unidade Executora', fieldName: 'periodoProjeto', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },
+    { label: 'Documento', fieldName: 'documento', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },  
+    { label: 'Validade', fieldName: 'validadeDocumento', hideDefaultActions: true, sortable: true, type: 'date-local',typeAttributes: {day: "2-digit", month: "2-digit"}, editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },  // entender qual info sera mostrada nessa coluna
+    { label: 'Operação', fieldName: 'operacao', hideDefaultActions: true, sortable: true, type: 'text', editable: true, cellAttributes: { alignment: 'left' } },
+]
+
+export default class Documentacao extends LightningElement {
+    
+    @api recordId;
+
+    @track columns = columns;
+    @track data;
+
+    @track isLoading;
+}
