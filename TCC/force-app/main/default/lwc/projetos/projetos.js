dd2cd4963b524ff9758ca227a138499c8d68381e
eliseu gili <gili.eliseup@gmail.com>
2022-10-15 17:06:25 -0300

    a

--- TCC/force-app/main/default/lwc/projetos/projetos.js
+++ TCC/force-app/main/default/lwc/projetos/projetos.js
@@ -1,21 +1,41 @@
 import { LightningElement, track, wire, api } from 'lwc';
-
-const columns = [
-    { label: 'Projeto', fieldName: 'nomeProjeto', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },
-    { label: 'Período', fieldName: 'periodoProjeto', hideDefaultActions: true, sortable: true, type: 'date-local',typeAttributes: {day: "2-digit", month: "2-digit"} , editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },
-    { label: 'Ações', fieldName: 'acoesProjeto', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },  // entender qual info sera mostrada nessa coluna
-    { label: 'Atendidos', fieldName: 'atendidosProjeto', hideDefaultActions: true, sortable: true, type: 'text', editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },  // entender qual info sera mostrada nessa coluna
-    { label: 'Valor', fieldName: 'valorProjeto', hideDefaultActions: true, sortable: true, type: 'currency', typeAttributes: { currencyCode: 'BRL' }, editable: false, displayReadOnlyIcon: true, cellAttributes: { alignment: 'left' } },
-]
+import getProjeto from '@salesforce/apex/ProjetosLWCController.getDadosProjetos';
+import { ShowToastEvent }  from 'lightning/platformShowToastEvent';
 
 export default class Projetos extends LightningElement {
 
     @api recordId;
 
-    @track columns = columns;
     @track data;
-
     @track isLoading;
 
-    
+    dataFim;
+
+    connectedCallback() {
+        getProjeto({recordId: this.recordId})
+        .then(result => {
+            this.data = result;
+            this.isLoading = false;
+
+            this.dataFim = this.data[0].dataTermino;
+        })
+        .catch(error => {
+            this.showToast('Ocorreu um erro', 'Recarregue a página e tente novamente', 'error', 'sticky');
+            this.data = undefined;
+        })
+    }
+
+    get showDataFim() {
+        return this.dataFim != null ? true : false;
+    }
+
+    showToast(title, message, variant, mode) {
+        const errorToast = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        });
+        this.dispatchEvent(errorToast);
+    }
 }
