427507b953e87bcbb0faabbc22b347ae5c1acfad
eliseu gili <gili.eliseup@gmail.com>
2022-11-26 13:12:30 -0300

    fileUploaderFinalizado

--- TCC/TCC/force-app/main/default/lwc/documentacaoTop/documentacaoTop.js
+++ TCC/TCC/force-app/main/default/lwc/documentacaoTop/documentacaoTop.js
@@ -1,8 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent }  from 'lightning/platformShowToastEvent';
 
 export default class DocumentacaoTop extends LightningElement {
 
+    @api recordId;
     isModalOpen = false
+    howManyFiles;
 
     openModal() {
         this.isModalOpen = true;
@@ -10,5 +13,26 @@ export default class DocumentacaoTop extends LightningElement {
 
     closeModal(event) {
         this.isModalOpen = false;
+    }
+
+    get acceptedFormats() {
+        return ['.csv','.doc','.xls','.pdf','.png','.jpg','.jpeg','.docx','.doc'];
+    }
+
+    handleUploadFinished(event){
+        const uploadedFiles = event.detail.files;
+        this.howManyFiles = event.detail.files.length;
+    }
+
+    handleClick(event){
+        this.closeModal(event);
+
+        const toast = new ShowToastEvent({
+            title: 'Sucesso',
+            message: this.howManyFiles + ' Arquivo(s) anexados com sucesso',
+            variant: 'success',
+            mode: 'dismissible'
+        });
+        this.dispatchEvent(toast); 
     }
 }
